8752bf14402de92c995ec67133c301893421f1e6
Toni Sredanović <toni@krakensystems.co>
2021-06-19 22:31:23 +0200

    ArtistAdmin

--- eardrops/core/admin.py
+++ eardrops/core/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
+from django.utils.html import format_html_join
+from django.utils.safestring import mark_safe
 
-from core.models import Song
+from core.models import *
 
 
 class SongAdmin(admin.ModelAdmin):
@@ -13,4 +15,30 @@ class SongAdmin(admin.ModelAdmin):
     ordering = ('-artist', '-title')
 
 
+class ArtistAdmin(admin.ModelAdmin):
+    model = Artist
+
+    list_display = ('name', 'songs_list', 'albums_list', )
+    list_display_links = ('name', )
+    search_fields = ('name', )
+    ordering = ('-name', )
+
+    @admin.display(description='Songs')
+    def songs_list(self, instance):
+        return format_html_join(
+            mark_safe('<br>'),
+            '{}',
+            ((line,) for line in [song_title for song_title in instance.songs.order_by('title').values_list('title', flat=True)]),
+        ) or mark_safe("<span class='errors'>No songs.</span>")
+    
+    @admin.display(description='Albums')
+    def albums_list(self, instance):
+        return format_html_join(
+            mark_safe('<br>'),
+            '{}',
+            ((line,) for line in [album_name for album_name in instance.albums.order_by('name').values_list('name', flat=True)]),
+        ) or mark_safe("<span class='errors'>No albums.</span>")
+
+
 admin.site.register(Song, SongAdmin)
+admin.site.register(Artist, ArtistAdmin)
