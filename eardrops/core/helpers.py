798718fadcb16c3b622acf2727fc29106dbcc4ac
Toni Sredanović <toni@krakensystems.co>
2021-06-21 23:40:36 +0200

    extract confirmation

--- eardrops/core/helpers.py
+++ eardrops/core/helpers.py
@@ -1,9 +1,19 @@
-def manually_set_value(value):
-    while True:
-        value = input('{}: '.format(value.capitalize()))
+def manually_set_value(field, default_value=None):
+    if default_value:
+        print('{}: {}'.format(field.capitalize(), default_value))
+        if confirm(field):
+            return default_value
 
-        confirmation = input('We Happy: ')
-        if confirmation in ['y', 'yes']:
+    while True:
+        value = input('{}: '.format(field.capitalize()))
+        if confirm(field):
             break
 
     return value
+
+def confirm(field):
+    confirmation = input('{} correct?(y/n)'.format(field.capitalize()))
+    if confirmation in ['y', 'yes']:
+        return True
+    else:
+        return False
