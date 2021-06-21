421e0902ecb51b7744c5a77ead18f810eac50d93
Toni Sredanović <toni@krakensystems.co>
2021-06-21 20:12:21 +0200

    logging

--- eardrops/eardrops/settings.py
+++ eardrops/eardrops/settings.py
@@ -130,3 +130,19 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = Path.joinpath(BASE_DIR, 'media/')
 MEDIA_ULR = '/media/'
+
+# Logging
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+}
